ed3f2bad79aa6302fa23a12b57f8301ed9a155e2
Dan O'Mahony <danomahony4@gmail.com>
2016-03-27 14:30:46 +0100

    Update Reverse a String.js

--- Reverse A String.js
+++ Reverse A String.js
@@ -7,9 +7,6 @@ Your result must be a string.
 Remember to use RSAP if you get stuck. Try to pair program. Write your own code.
 Here are some helpful links:
 Global String Object String.split() Array.reverse() Array.join()
-Code by Rafael Rodriguez
-rafase282@gmail.com
-http://www.freecodecamp.com/rafase282
 */
 
 function reverseString(str) {
